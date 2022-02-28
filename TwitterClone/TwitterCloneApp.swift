1c3b9562994f5125b963ed70ae80f6ab83bf8991
George <51861795+gloulakis@users.noreply.github.com>
2022-02-28 20:40:37 +0200

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Georgios Loulakis on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
